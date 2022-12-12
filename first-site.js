c30f58163f080a29766e224c664fc25007daf224
brandanblevins <brandan.blevins@gmail.com>
2022-12-12 13:49:27 -0700

    Check in rock paper scissors progress

--- first-site.js
+++ first-site.js
@@ -1,3 +1,14 @@
-function getComputerChoice(Rock, Paper, Scissors) {
+function getComputerChoice;
 
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    let playerSelection = prompt('Rock, Paper, or Scissors?', '');
+    let computerSelection = (rock, paper, scissors);
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+        if
+    }
 }
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
